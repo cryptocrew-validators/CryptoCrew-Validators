03cc648f8a33a87e2c5e6ad1dfdf961ce6dd6236
clemensgg <csc@soundportal.at>
2023-04-18 12:47:35 +0200

    fix src chain id

--- update_relayer_md.js
+++ update_relayer_md.js
@@ -23,16 +23,16 @@ function generateMDTable(relayers, chains) {
     const chainId = chain.chain_id;
     const channels = relayerChains[chainId];
     if (channels) {
-      const mdContent = generateMDContent(channels);
+      const mdContent = generateMDContent(channels, chain);
       const outputPath = path.join('chains', chain.name, 'service_ibc_relayer.md');
       fs.writeFileSync(outputPath, mdContent);
     }
   }
 }
 
-function generateMDContent(channels) {
+function generateMDContent(channels, chain) {
   const header = '## CryptoCrew IBC relayer\n\n| src_chain | dst_chain | IBC port | IBC channel |\n| --------------- | --------------- | ------------ | -------------- |\n';
-  const rows = channels.map(channel => `| ${channel.chain_id} | ${channel.dst_chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n');
+  const rows = channels.map(channel => `| ${chain.chain_id} | ${channel.dst_chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n');
 
   return header + rows;
 }
