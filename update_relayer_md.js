533f5cbe88eb1f53ee67d245abc47fad52fe194e
clemensgg <csc@soundportal.at>
2023-04-18 18:52:48 +0200

    format

--- update_relayer_md.js
+++ update_relayer_md.js
@@ -2,60 +2,60 @@ const fs = require('fs');
 const path = require('path');
 
 function readJSONFile(filePath) {
-  const rawData = fs.readFileSync(filePath);
-  return JSON.parse(rawData);
+    const rawData = fs.readFileSync(filePath);
+    return JSON.parse(rawData);
 }
 
 function generateMDTable(relayers, chains) {
-  const relayerChains = {};
+    const relayerChains = {};
 
-  for (const relayer of relayers.relayers) {
-    for (const chain of relayer.chains) {
-      const chainId = chain.chain_id;
-      if (!relayerChains[chainId]) {
-        relayerChains[chainId] = [];
-      }
-      relayerChains[chainId].push(...chain.channels);
+    for (const relayer of relayers.relayers) {
+        for (const chain of relayer.chains) {
+            const chainId = chain.chain_id;
+            if (!relayerChains[chainId]) {
+                relayerChains[chainId] = [];
+            }
+            relayerChains[chainId].push(...chain.channels);
+        }
     }
-  }
 
-  for (const chain of chains.chains) {
-    const chainId = chain.chain_id;
-    const channels = relayerChains[chainId];
-    const dstChannels = findDstChannels(relayers, chainId);
+    for (const chain of chains.chains) {
+        const chainId = chain.chain_id;
+        const channels = relayerChains[chainId];
+        const dstChannels = findDstChannels(relayers, chainId);
 
-    if (channels || dstChannels.length > 0) {
-      const mdContent = generateMDContent(channels, dstChannels, chain);
-      const outputPath = path.join('chains', chain.name, 'service_ibc_relayer.md');
-      fs.writeFileSync(outputPath, mdContent);
+        if (channels || dstChannels.length > 0) {
+            const mdContent = generateMDContent(channels, dstChannels, chain);
+            const outputPath = path.join('chains', chain.name, 'service_ibc_relayer.md');
+            fs.writeFileSync(outputPath, mdContent);
+        }
     }
-  }
 }
 
 function findDstChannels(relayers, dstChainId) {
-  const dstChannels = [];
+    const dstChannels = [];
 
-  for (const relayer of relayers.relayers) {
-    for (const chain of relayer.chains) {
-      for (const channel of chain.channels) {
-        if (channel.dst_chain_id === dstChainId) {
-          dstChannels.push({
-            ...channel,
-            chain_id: chain.chain_id
-          });
+    for (const relayer of relayers.relayers) {
+        for (const chain of relayer.chains) {
+            for (const channel of chain.channels) {
+                if (channel.dst_chain_id === dstChainId) {
+                    dstChannels.push({
+                        ...channel,
+                        chain_id: chain.chain_id
+                    });
+                }
+            }
         }
-      }
     }
-  }
 
-  return dstChannels;
+    return dstChannels;
 }
 
 function generateMDContent(srcChannels, dstChannels, chain) {
     const wallets = chain.relayer_accounts && chain.relayer_accounts.length > 0
-    ? `Active Relayer Wallets:\n\n${chain.relayer_accounts.map(wallet => `- ${wallet}`).join('\n')}\n\n`
-    : '';
-  const header = `## CryptoCrew IBC relayer
+        ? `Active Relayer Wallets:\n\n${chain.relayer_accounts.map(wallet => `- \`${wallet}\``).join('\n')}\n\n`
+        : '';
+    const header = `## CryptoCrew IBC relayer
 IBC relayers play a crucial role in the interchain by efficiently managing and transmitting data and assets between different blockchain networks using the Inter-Blockchain Communication (IBC) protocol.
 
 To facilitate interchain message transfers, CryptoCrew utilizes the following IBC relayer software: 
@@ -65,10 +65,10 @@ To facilitate interchain message transfers, CryptoCrew utilizes the following IB
 ${wallets}### Active IBC channels \`` + chain.name + `\`
 | src_chain | dst_chain | IBC port | IBC channel |
 | --------------- | --------------- | ------------ | -------------- |\n`;
-  const srcRows = srcChannels ? srcChannels.map(channel => `| ${chain.chain_id} | ${channel.dst_chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n') : '';
-  const dstRows = dstChannels.map(channel => `| ${channel.chain_id} | ${chain.chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n');
+    const srcRows = srcChannels ? srcChannels.map(channel => `| ${chain.chain_id} | ${channel.dst_chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n') : '';
+    const dstRows = dstChannels.map(channel => `| ${channel.chain_id} | ${chain.chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n');
 
-  return header + srcRows + (srcRows && dstRows ? '\n' : '') + dstRows;
+    return header + srcRows + (srcRows && dstRows ? '\n' : '') + dstRows;
 }
 
 const relayers = readJSONFile('relayers.json');
