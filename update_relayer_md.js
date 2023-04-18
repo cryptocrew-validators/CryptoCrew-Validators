441e66b3e3abbc93aa21febce5979dfbf9454611
clemensgg <csc@soundportal.at>
2023-04-18 12:40:44 +0200

    add ibc relayer service - update md workflow

---
+++ update_relayer_md.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+function readJSONFile(filePath) {
+  const rawData = fs.readFileSync(filePath);
+  return JSON.parse(rawData);
+}
+
+function generateMDTable(relayers, chains) {
+  const relayerChains = {};
+
+  for (const relayer of relayers.relayers) {
+    for (const chain of relayer.chains) {
+      const chainId = chain.chain_id;
+      if (!relayerChains[chainId]) {
+        relayerChains[chainId] = [];
+      }
+      relayerChains[chainId].push(...chain.channels);
+    }
+  }
+
+  for (const chain of chains.chains) {
+    const chainId = chain.chain_id;
+    const channels = relayerChains[chainId];
+    if (channels) {
+      const mdContent = generateMDContent(channels);
+      const outputPath = path.join('chains', chain.name, 'service_ibc_relayer.md');
+      fs.writeFileSync(outputPath, mdContent);
+    }
+  }
+}
+
+function generateMDContent(channels) {
+  const header = '## CryptoCrew IBC relayer\n\n| src_chain | dst_chain | IBC port | IBC channel |\n| --------------- | --------------- | ------------ | -------------- |\n';
+  const rows = channels.map(channel => `| ${channel.chain_id} | ${channel.dst_chain_id} | ${channel.port_id} | ${channel.channel_id} |`).join('\n');
+
+  return header + rows;
+}
+
+const relayers = readJSONFile('relayers.json');
+const chains = readJSONFile('chains.json');
+generateMDTable(relayers, chains);
