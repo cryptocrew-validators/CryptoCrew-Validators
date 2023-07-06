16a4a146ce5aa47b7dae7884c79c664cc270625d
ccv-automation <ccv2@ccvalidators.com>
2023-07-06 11:28:02 +0000

    auto update service: /home/node/service_Node_Snapshot.md

--- chains/empowerchain/download_snapshot.sh
+++ chains/empowerchain/download_snapshot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-URL="https://dl.ccvalidators.com/SNAPSHOTS/$CHAIN_NAME/$SNAPSHOT"
+URL="https://dl.ccvalidators.com/SNAPSHOTS/empowerchain/empowerchain-1_122347.tar.lz4"
 cd /home/empowerchain/.empowerd
 cp data/priv_validator_state.json ./priv_validator_state.json.tmp
 rm -rf data
-wget -O - $URL | tee >(sha256sum > `basename $URL`.sha256_check) | lz4 -d | tar -xvf -
-diff <(sha256sum `basename $URL` | cut -d " " -f 1) <(cut -d " " -f 1 `basename $URL`.sha256_check)
+wget -O - $URL | tee >(sha256sum > `basename $URL`.sha256) | lz4 -d | tar -xvf -
+diff <(sha256sum `basename $URL` | cut -d " " -f 1) <(cut -d " " -f 1 `basename $URL`.sha256)
 mv ./priv_validator_state.json.tmp data/priv_validator_state.json
 sudo systemctl start empowerd
